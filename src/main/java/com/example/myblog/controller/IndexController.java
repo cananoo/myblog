1c1027b0694a9dd9288d3d8df8d6db57c5e8818c
root <2059997196@qq.com>
2023-08-20 21:22:09 +0800

    design (Page-handle)

--- src/main/java/com/example/myblog/controller/IndexController.java
+++ src/main/java/com/example/myblog/controller/IndexController.java
@@ -1,18 +1,21 @@
 package com.example.myblog.controller;
 
-import com.example.myblog.exception.UserTooManyException;
-import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable("id") Integer id,@PathVariable("name") String name) { //SpringMVC支持路径中的占位符
+    @GetMapping("/")
+    public String index() {
 
-        System.out.println("index");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+
+        return "blog";
     }
 }
